bd5c04ec27caf5c44d0be30002d62baa80859c75
JoaquinG98 <joaquin4525@hotmail.com>
2021-09-04 22:08:54 -0300

    Primer commit

---
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-ver-pais',
+  templateUrl: './ver-pais.component.html',
+  styles: [
+  ]
+})
+export class VerPaisComponent implements OnInit {
+
+  pais!: Country;
+
+  constructor(private rutaActivada: ActivatedRoute,
+              private paisService: PaisService) { }
+
+  ngOnInit(): void {
+    this.rutaActivada.params
+        .pipe(
+          switchMap(({id}) => this.paisService.getPaisPorCodigo(id)),
+          tap(console.log)
+        )
+        .subscribe(pais => this.pais = pais);
+
+    // this.rutaActiviada.params
+    //     .subscribe(({id}) => {
+    //       console.log(id);
+    //       this.paisService.getPaisPorCodigo(id)
+    //           .subscribe(pais => {
+    //             console.log(pais);
+    //           })
+    //     })
+  }
+
+}
